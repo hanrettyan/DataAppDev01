3747a20a5f5bbb9f77606037d4cc2199776ccd61
hanrettyan <47253726+hanrettyan@users.noreply.github.com>
2019-10-01 14:00:58 -0400

    updates
    updates

--- DataApp1/DataAppDev01.R
+++ DataApp1/DataAppDev01.R
@@ -1,4 +1,14 @@
-# Attempting to bring everything over from the Rmd into a Shiny app to support real-time data updates within running application.
+# THIS APP IS ATTEMPTING TO CREATE A DASHBOARD THAT HOSTS A DATATABLE AND LEAFLET FOR DATA IN EACH DIFFERENT CLASS TAB
+#
+# NOTES: Cannot place same datatableoutput, leafletoutput on more than one page inside dashboard!!! This should only be an issue
+# when trying to use sample data to develop app.
+# Raster image contained within Shiny app folder- not programmed to pull from alt data source at this time. ARE DISABLED IN MAPS
+# TEMPORARILY TO INCREASE PROCESSING TIME FOR APP DEVELOPMENT.
+
+
+
+
+
 
 # Load necessary libraries
 library(shiny)
@@ -10,71 +20,62 @@ library(dplyr)
 library(shinydashboard)
 
 # Load data (using different sets of sample data)
-classdata <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRFdDGgmoI-9prZL45gHOixA4thITZleI_DkEZ49E-JqELRaxn8K46YM1HaBb0bBgkV5Xx-YrxKRgYM/pub?output=csv")
+classesdata <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRFdDGgmoI-9prZL45gHOixA4thITZleI_DkEZ49E-JqELRaxn8K46YM1HaBb0bBgkV5Xx-YrxKRgYM/pub?output=csv")
 araptusdata <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQNCFb3C1oSia_dN5ISXusrGqwVSFibt0_zkqq7wiNtW_tl1DM-Ch-fIKKmIz_ijXxdrKux6qvvy8yD/pub?output=csv")
-# rast <- raster("alt_22.tif")
-# Currently getting error; cannot create RasterLayer; file does not exist
+rast <- raster("alt_22.tif")
 
-# A custom marker for fun map
-bluepointicon <- makeIcon(
-    iconUrl = "https://www.twinword.com/wp-content/uploads/2016/10/location-icon-vector-blue-pin.svg",
-    iconWidth = 30,
-    iconHeight = 30
-)
 
 #################
 ###### UI #######
 #################
 
-ui <- dashboardPage(
-    
-    # Page header/app title
-    dashboardHeader(title = "Testing Crosstalk", color="teal"),
-    
-    # Sidebar/list of classes to open pages
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("ENVS 601", tabName = "envs601", icon = icon("dashboard")),
-            menuItem("ENVS 602", tabName = "envs602", icon = icon("dashboard")),
-            menuItem("ENVS 603", tabName = "envs603", icon = icon("dashboard"))
-        )
-    ),
-    
-    # Body of pages
-    dashboardBody(
-        tabItems(
-            #ENVS 601 page
-            tabItem(tabName = "envs601",
-                    fluidPage(
-                        box(title = "Data",
-                            dataTableOutput("araptusdatatable")),
-                        box(title = "Map",
-                            leafletOutput("araptusmap"))
-                    )
-            ),
-            
-            #ENVS 602 page
-            tabItem(tabName = "envs602",
-                    fluidPage(
-                        box(title = "Data",
-                            dataTableOutput("classdatatable"))
-                        # box(title = "Map",
-                            # leafletOutput())
-                            )),
-            
-            #ENVS 603 page
-            tabItem(tabName = "envs603",
-                    fluidPage(
-                        box(title = "Data",
-                            dataTableOutput("araptusdatatable")),
-                        box(title = "Map",
-                            leafletOutput("araptusmap"))
-                    ))
-                    ))
-        )
-    
+ui <- dashboardPage(skin = "yellow",
+                    
+                    # Page header/app title
+                    dashboardHeader(title = "Testing Shiny App"),
+                    
+                    # Sidebar/list of classes to open pages
+                    dashboardSidebar(
+                        sidebarMenu(
+                            menuItem("ENVS 601", tabName = "envs601", icon = icon("dashboard")),
+                            menuItem("ENVS 602", tabName = "envs602", icon = icon("dashboard")),
+                            menuItem("ENVS 603", tabName = "envs603", icon = icon("dashboard"))
+                        )
+                    ),
+                    
+                    # Body of pages
+                    dashboardBody(
+                        tabItems(
+                            #ENVS 601 page
+                            tabItem(tabName = "envs601",
+                                    fillPage(
+                                       title = "Envs 601 data",
+                                       dataTableOutput("araptusdatatable"),
+                                       leafletOutput("araptusmap"))),
 
+                            
+                            #ENVS 602 page
+                            tabItem(tabName = "envs602",
+                                    fillPage(
+                                        title = "Envs 602 data",
+                                        dataTableOutput("classesdatatable"))),
+                            
+                            #ENVS 603 page
+                            tabItem(tabName = "envs603",
+                                    fillPage(
+                                        title = "Envs 603 data",
+                                        dataTableOutput("araptusdatatable2"),
+                                        leafletOutput("araptusmap2")))
+                            
+                            
+                            
+                            
+                        )
+                    ))
+                    
 
+                            
+                            
 #################
 #### SERVER #####
 #################
@@ -91,37 +92,44 @@ server <- function(input, output) {
                                                   class = "compact",
                                                   height = "100%",
                                                   options = list(
-                                                      # dom = "Blrtip",
-                                                      deferRender = TRUE,
-                                                      scrollY = 10,
-                                                      scroller = TRUE,
+                                                      lengthChange = FALSE,
+                                                      dom = "Blrtip",
+                                                      autoWidth = TRUE,
+                                                      # deferRender = TRUE,
+                                                      # scrollY = 10,
+                                                      scrollX = 30,
+                                                      # scroller = TRUE,
                                                       columnDefs = list(
                                                           list(
                                                               visible = TRUE)),
                                                       buttons = list(
                                                           "csv", "excel")),
-                                                  colnames = c("Site Number" = "Site",
+                                                  colnames = c("Site" = "Site",
                                                                "Long" = "Longitude",
                                                                "Lat" = "Latitude",
                                                                "No. Males" = "Males",
                                                                "No. Females" = "Females",
                                                                "Suitability" = "Suitability"))
     
+    
+    
     # Output for ARAPTUS leaflet:
     output$araptusmap <- renderLeaflet({
         leaflet(araptusdata) %>%
             addProviderTiles( providers$Esri.WorldImagery, group = "Imagery") %>%
             
-            addMarkers( icon = bluepointicon, label = ~Site, group = "Araptus Data") %>%
+            # addRasterImage(rast, opacity = 0.5, group = "Elevation Raster") %>%
+            
+            addMarkers( label = ~Site, group = "Araptus Data") %>%
             
             addLayersControl(
                 overlayGroups = c("Elevation Raster", "Araptus Data"),
                 options = layersControlOptions(collapsed = FALSE)
             )
     })
-
+    
     # Output for CLASSES datatable:
-    output$classesdatatable = DT::renderDataTable({classdata},
+    output$classesdatatable = DT::renderDataTable({classesdata},
                                                   filter="top",
                                                   fillContainer = TRUE,
                                                   extensions = c("Buttons",
@@ -131,23 +139,77 @@ server <- function(input, output) {
                                                   class = "compact",
                                                   height = "100%",
                                                   options = list(
-                                                      # dom = "Blrtip",
+                                                      lengthChange = FALSE,
+                                                      dom = "Blrtip",
                                                       deferRender = TRUE,
-                                                      scrollY = 10,
-                                                      scroller = TRUE,
+                                                      # scrollY = 10,
+                                                      scrollX = 30,
+                                                      # scroller = TRUE,
                                                       columnDefs = list(
                                                           list(
                                                               visible = TRUE)),
                                                       buttons = list(
                                                           "csv", "excel")),
-                                                  colnames = c("Class Nbr" = "CLASS NUMBER",
-                                                               "Class Name" = "NAME",
+                                                  colnames = c("Class Num" = "CLASS.NUMBER",
+                                                               "Name" = "CLASS.NAME",
                                                                "Grade" = "GRADE",
                                                                "Year" = "YEAR",
-                                                               "Semester" = "SEMESTER"))
+                                                               "Semester" = "SEMESTER")
+    )
     
     
-}
+    # Output for ARAPTUS datatable 2:
+    output$araptusdatatable2 = DT::renderDataTable({araptusdata},
+                                                  filter="top",
+                                                  fillContainer = TRUE,
+                                                  extensions = c("Buttons",
+                                                                 "Scroller"),
+                                                  rownames = FALSE,
+                                                  style = "bootstrap",
+                                                  class = "compact",
+                                                  height = "100%",
+                                                  options = list(
+                                                      lengthChange = FALSE,
+                                                      dom = "Blrtip",
+                                                      autoWidth = TRUE,
+                                                      # deferRender = TRUE,
+                                                      # scrollY = 10,
+                                                      scrollX = 30,
+                                                      # scroller = TRUE,
+                                                      columnDefs = list(
+                                                          list(
+                                                              visible = TRUE)),
+                                                      buttons = list(
+                                                          "csv", "excel")),
+                                                  colnames = c("Site" = "Site",
+                                                               "Long" = "Longitude",
+                                                               "Lat" = "Latitude",
+                                                               "No. Males" = "Males",
+                                                               "No. Females" = "Females",
+                                                               "Suitability" = "Suitability"))
+    
+    
+    
+    # Output for ARAPTUS leaflet 2:
+    output$araptusmap2 <- renderLeaflet({
+        leaflet(araptusdata) %>%
+            addProviderTiles( providers$Esri.WorldImagery, group = "Imagery") %>%
+            
+            # addRasterImage(rast, opacity = 0.5, group = "Elevation Raster") %>%
+            
+            addMarkers( label = ~Site, group = "Araptus Data") %>%
+            
+            addLayersControl(
+                overlayGroups = c("Elevation Raster", "Araptus Data"),
+                options = layersControlOptions(collapsed = FALSE)
+            )
+    })
+    
+    
+    
+    
+    
+    }
 
 #################
 #### RUN APP ####
